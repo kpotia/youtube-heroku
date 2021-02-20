4196145291e6dfa375dab92e709c724d2c62cb95
Mr Kpoti <patrickamtepe@gmail.com>
2021-02-20 07:32:42 +0000

    initial commit

---
+++ src/Twig/YoutubeExtension.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+use RicardoFiorani\Matcher\VideoServiceMatcher;
+
+
+class YoutubeExtension extends AbstractExtension
+{
+
+    private $youtubeParser;
+
+    function __construct()
+    {
+        $this->youtubeParser = new VideoServiceMatcher();
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
+            new TwigFilter('youtube_thumbnail', [$this, 'youtubeThumbnail']),
+            new TwigFilter('youtube_player', [$this, 'youtubePlayer']),
+        ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('function_name', [$this, 'doSomething']),
+        ];
+    }
+
+    public function youtubeThumbnail($url)
+    {
+        $video  = $this->youtubeParser->parse($url);
+        return $video->getLargestThumbnail();     
+    }   
+
+    public function youtubePlayer($url)
+    {
+        $video = $this->youtubeParser->parse($url);
+        return $video->getEmbedCode('100%', 500, true, true);
+    }
+}
